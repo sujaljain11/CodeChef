735c8bbd5b2ca0f43a2a5aa975d416d7723bef49
sujaljain11 <jain11rahi@gmail.com>
2022-09-09 23:43:04 +0530

    codes

---
+++ Good_Pairs_Long Challenge.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define pi (3.141592653589)
+#define ll long long int
+#define rrep(i, n) for (ll i = n - 1; i >= 0; i--)
+#define lp(i, a, b) for (ll i = a; i < b; i++)
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define rep1(i, n) for (ll i = 1; i < n; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ps(x, y) fixed << setprecision(y) << x
+#define it(i, f) for (auto i = f.begin(); i != f.end(); i++)
+#define pii pair<int, int>
+#define vi vector<int>
+using namespace std;
+
+void solution()
+{
+    ll n, ans = 0;
+    cin >> n;
+    ll v[n];
+    map<ll, ll> mp;
+    rep(i, n)
+    {
+        cin >> v[i];
+        mp[v[i]]++;
+    }
+
+    for (auto it : mp)
+    {
+        ll x = it.second;
+        ans += x * (x - 1) / 2;
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    fast int t;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
