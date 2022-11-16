10d09dd8197dd54be1019e1bfeaadf8f88ef139c
sujaljain11 <jain11rahi@gmail.com>
2022-11-16 19:21:57 +0530

    rcodes

---
+++ A_Competitive_Programmer.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#define pi (3.141592653589)
+#define pb push_back
+#define pp pop_back
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define ll long long int
+#define rrep(i, n) for(ll i=n-1;i>=0;i--)
+#define lp(i,a,b) for(ll i=a;i<b;i++)
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rep1(i,n) for(ll i=1;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define it(i,f) for(auto i:f)
+
+const ll sz=1e6+1;
+const ll mod=1e9+7;
+
+using namespace std;
+
+typedef pair<long long int, long long int> pii;
+typedef vector<long long int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<long long int,long long int> mp;
+typedef unordered_map<long long int,long long int> ump;
+typedef set<long long int> seti;
+typedef multiset<long long int> mset;
+
+void solution()
+{
+    string s;
+    cin>>s;
+    bool flag1=false,flag2=false;
+    ll l=s.length();
+    ll i=0,sum=0,cn=0;
+    while(i<l)
+    {
+        ll x=s[i]-'0';
+        if(x==0) 
+        {
+            flag1=true;
+            cn++;
+        }
+        else if(x%2==0) flag2=true;
+        sum+=x%mod;
+        sum%=mod;
+        i++;
+    }
+    if(cn==s.length()) 
+    {
+        cout<<"red"<<endl;
+        return;
+    }
+    if(cn==1)
+    {
+        if(sum%3==0 && flag1 && flag2) cout<<"red"<<endl;
+        else cout<<"cyan"<<endl;
+        return;
+    }
+    else if(sum%3==0 && flag1) 
+    {
+        cout<<"red"<<endl;
+        return;
+    }
+    cout<<"cyan"<<endl;
+}
+
+
+int32_t main()
+{
+    fast
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solution();
+    }
+    return 0;
+}
---
+++ A_Young_Physicist.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define pi (3.141592653589)
+#define pb push_back
+#define pp pop_back
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define ll long long int
+#define rrep(i, n) for(ll i=n-1;i>=0;i--)
+#define lp(i,a,b) for(ll i=a;i<b;i++)
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rep1(i,n) for(ll i=1;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define it(i,f) for(auto i:f)
+
+const ll sz=1e6+1;
+const ll mod=1e9+7;
+
+using namespace std;
+
+typedef pair<long long int, long long int> pii;
+typedef vector<long long int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<long long int,long long int> mp;
+typedef unordered_map<long long int,long long int> ump;
+typedef set<long long int> seti;
+typedef multiset<long long int> mset;
+
+void solution()
+{
+    
+}
+
+
+int32_t main()
+{
+    fast
+    ll n,sum1=0,sum2=0,sum3=0;
+    cin>>n;
+    vi a(n),b(n),c(n);
+    rep(i,n) cin>>a[i]>>b[i]>>c[i];
+    it(i,a) sum1+=i;
+    it(i,b) sum2+=i;
+    it(i,c) sum3+=i;
+    if(sum1==0 && sum2==0 && sum3==0) py;
+    else pn;
+    return 0;
+}
