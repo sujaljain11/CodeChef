97884f46999be1c072688f1ff0a60b100f4cae4e
sujaljain11 <jain11rahi@gmail.com>
2022-11-19 23:44:17 +0530

    rcodes

---
+++ B_String_LCM.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#define pi (3.141592653589)
+#define pb push_back
+#define pp pop_back
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define ll long long int
+#define rrep(i, n) for(ll i=n-1;i>=0;i--)
+#define lp(i,a,b) for(ll i=a;i<b;i++)
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rep1(i,n) for(ll i=1;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define it(i,f) for(auto i:f)
+
+const ll sz=1e6+1;
+const ll mod=1e9+7;
+
+using namespace std;
+
+typedef pair<long long int, long long int> pii;
+typedef vector<long long int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<long long int,long long int> mp;
+typedef unordered_map<long long int,long long int> ump;
+typedef set<long long int> seti;
+typedef multiset<long long int> mset;
+
+string func(string p,ll q)
+{
+    string res="";
+    while(q--) res+=p;
+    return res;
+}
+
+void solution()
+{
+    string a,b;
+    cin>>a>>b;
+    unsigned int x=a.length(),y=b.length();
+    unsigned int g=__gcd(x,y);
+    if(func(a,y/g)==func(b,x/g))
+    {
+        cout<<func(a,y/g)<<endl;
+    }
+    else cout<<-1<<endl;
+}
+
+
+int32_t main()
+{
+    fast
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solution();
+    }
+    return 0;
+}
